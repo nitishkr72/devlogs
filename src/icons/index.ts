5ec53e7c2784eb0e7b8360226f36c659eb8c845d
Nitish Kumar <nitish_tu@outlook.com>
2024-10-20 14:09:57 +0530

    feat: header implemented

---
+++ src/icons/index.ts
@@ -0,0 +1,7 @@
+export const INSTAGRAM_PNG =
+	"https://static.cdninstagram.com/rsrc.php/v3/yq/r/_7cAFFbc4Pr.png";
+export const FACEBOOK_PNG =
+	"https://z-m-static.xx.fbcdn.net/rsrc.php/v3/yD/r/5D8s-GsHJlJ.png";
+export const GITHUB_PNG =
+	"https://www.svgrepo.com/show/303615/github-icon-1-logo.svg";
+export const LEETCODE_PNG = "https://www.svgrepo.com/show/306328/leetcode.svg";
